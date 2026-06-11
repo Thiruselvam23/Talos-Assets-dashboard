6d9c25aede0ec68d999c0c10903a9614c5133b3f
Thiruselvam P <thiruselvampanneer@gmail.com>
2026-06-11 13:09:09 +0530

    update nows

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://talos-assets-dashboard.vercel.app/",
+        "https://talos-assets-dashboard-2xxr.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
